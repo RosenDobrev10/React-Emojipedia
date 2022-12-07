613376cc450ee8a01bda1d665dbaeff967e9299a
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-12-07 16:53:35 +0200

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+    {
+        // id: 1,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        // id: 2,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning:
+            "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+        // id: 3,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        meaning:
+            "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        // id: 4,
+        emoji: "❤️",
+        name: "Red Heart",
+        meaning:
+            "A classic red love heart emoji, used for expressions of love and romance. This is the most popular heart emoji A similar emoji exists for the heart suit in a deck of playing cards."
+    },
+    {
+        // id: 5,
+        emoji: "😘",
+        name: "Face Blowing a Kiss",
+        meaning:
+            "A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally."
+    },
+    {
+        // id: 6,
+        emoji: "🤪",
+        name: "Zany Face",
+        meaning:
+            "A yellow face with its head tilted, its tongue hanging out of a big grin, and wide eyes in a wild, cockeyed expression. Generally used to express silliness."
+    }
+];
+
+export default emojipedia;
